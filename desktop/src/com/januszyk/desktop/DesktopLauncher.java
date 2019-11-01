8c6999068cafae7696159b876d0442f78555677e
dulcerito <erik.januszyk@gmail.com>
2019-11-01 17:11:48 +0100

    Initialize libgdx project

---
+++ desktop/src/com/januszyk/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.januszyk.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.januszyk.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
